80dcbba585d1046538ee2181a1f50e93bddb878c
ericliuhusky <289471838@qq.com>
2021-12-22 18:20:15 +0800

    :sparkles: 添加OC混编支持

---
+++ Demo/DemoOC.m
@@ -0,0 +1,22 @@
+//
+//  DemoOC.m
+//  Demo
+//
+//  Created by lzh on 2021/12/22.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface DemoOC : NSObject
+
++ (void)run;
+
+@end
+
+@implementation DemoOC
+
++ (void)run {
+    
+}
+
+@end
